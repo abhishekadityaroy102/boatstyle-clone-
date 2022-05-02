75b8e81dba2d405e1b8963463be9ccfb532958fc
Kundan <kd181197@gmail.com>
2022-05-02 23:37:53 +0530

    navbar added

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+function navbar(){
+    return`
+    <div id="logo">
+    <img src="https://gumlet.assettype.com/swarajya%2F2021-01%2F7a6e91f6-5862-4ac5-bb08-eb5bf2a3e20c%2FboAt_logo.jpg?q=75&auto=format%2Ccompress&w=1200" alt="">
+</div>
+<div id="pages">
+    <a href="">Shop</a>
+    <a href="">Sail with boat</a>
+    <a href="">More</a>
+</div>
+<div id="elements">
+    <input type="text" id="search" placeholder="Search...">
+    <i class="fa-solid fa-user"></i>
+    <i class="fa-solid fa-gifts"></i>
+    <i class="fa-solid fa-cart-shopping"></i>
+</div>`
+}
+export {navbar};
